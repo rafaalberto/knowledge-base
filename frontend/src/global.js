f8f8c80d470d94f0f1203ac034cde2e871815789
Rafael Alberto <s2it_ralberto@uolinc.com>
2019-01-23 17:11:15 -0200

    atualização

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 
+export const userKey = '__knowledge_user'
 export const baseApiUrl = 'http://localhost:3000'
 export function showError(error) {
     if(error && error.response && error.response.data) {
@@ -11,4 +12,4 @@ export function showError(error) {
     }
 }
 
-export default { baseApiUrl, showError }
+export default { baseApiUrl, showError, userKey }
